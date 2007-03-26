7051de205f6a544e7a0f6311c6b46709efcc3557
Dylan Cochran <heliocentric@gmail.com>
2007-03-26 05:10:55 +0000

    Initial Import
    Most of this is horrible, and more a POC then anything else.
    
    currently can generate a basic links.conf and a basic server.conf
    
    
    git-svn-id: http://gs-config.googlecode.com/svn/trunk@2 b706ddbe-c0eb-11de-acfc-b766374499fb

---
+++ include/conf-gen.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+NAME=./network.conf
+OUTPUTPATH=/tmp/cf-gen
+rm -rf /tmp/cf-gen
+mkdir -p /tmp/cf-gen
+chmod 750 /tmp/cf-gen
+
+# stripping out comments and whitespace:
+grep -v ^# ${NAME} | grep -v ^$ | cut -f 1 -d \# > ${OUTPUTPATH}/network.conf
+STRIPCONF=${OUTPUTPATH}/network.conf
+mkdir -p ${OUTPUTPATH}/conf
+NETWORK=area51
+
+for WORKINGSERVER in `grep ^S ${STRIPCONF}`
+do
+	SERVERNAME=`echo ${WORKINGSERVER} | cut -f 2 -d :`
+	CONFPATH=${OUTPUTPATH}/conf/${SERVERNAME}
+	PORTSFILE=${CONFPATH}/ports.conf
+	LINKFILE=${CONFPATH}/links.conf
+	SERVERFILE=${CONFPATH}/server.conf
+	echo "Generating config files for ${SERVERNAME}.${NETWORK}"
+	mkdir ${CONFPATH}
+	BINDIP=`echo ${WORKINGSERVER} | cut -f 3 -d : | cut -f 1 -d -`
+	echo "\t- starting ${LINKFILE}"
+	for REMOTESERVER in `grep ^S ${STRIPCONF} | grep -v ${SERVERNAME}`
+	do
+		LINKNAME=`echo ${REMOTESERVER} | cut -f 2 -d :`
+		LINKIP=`echo ${REMOTESERVER} | cut -f 3 -d : | cut -f 1 -d -`
+		echo "\nlink ${LINKNAME}.${NETWORK} {" >> ${LINKFILE}
+		echo "\tusername *;" >> ${LINKFILE}
+		echo "\thostname ${LINKIP};" >> ${LINKFILE}
+		echo "\tbind-ip ${BINDIP};" >> ${LINKFILE}
+		echo "\tport 4400;" >> ${LINKFILE}
+		echo "\thub *;" >> ${LINKFILE}
+		echo "\tpassword-connect \"wyldryde-l33t-link-password\";" >> ${LINKFILE}
+		echo "\tpassword-recieve \"\$MMeriLxK\$lVYlZHHBGNvcZcZEBw1d/w==\" { md5; };" >> ${LINKFILE}
+		echo "\tclass leaf;" >> ${LINKFILE}
+		echo "\toptions {" >> ${LINKFILE}
+		echo "\t};" >> ${LINKFILE}
+		echo "};\n" >> ${LINKFILE}
+	done
+	echo "\t- ending ${LINKFILE}"
+	echo "\t- starting ${PORTSFILE}"
+	for LISTENIP in `echo ${WORKINGSERVER} | cut -f 3 -d : | sed s/-/\ /g`
+	do
+		for PORTS in `grep ^P ${STRIPCONF}`
+		do
+			PORT=`echo ${PORTS} | cut -f 2 -d :`
+			OPTIONS=`echo ${PORTS} | cut -f 3 -d :`
+			echo "listen ${LISTENIP}:${PORT} {" >> ${PORTSFILE}
+			if [ "${OPTIONS}" != "" ] ; then
+				echo "\toptions {" >> ${PORTSFILE}
+				case ${OPTIONS} in *c*) echo "\t\tclients-only;" >> ${PORTSFILE} ;;	esac
+				case ${OPTIONS} in *s*) echo "\t\tservers-only;" >> ${PORTSFILE} ;;	esac
+				case ${OPTIONS} in *l*) echo "\t\tssl;" >> ${PORTSFILE} ;;		esac
+				echo "\t};" >> ${PORTSFILE}
+			fi
+			echo "};" >> ${PORTSFILE}
+		done
+	done
+	echo "\t- ending ${PORTSFILE}"
+	echo "\t- starting ${SERVERFILE}"
+		echo "me {" >> ${SERVERFILE}
+		echo "\tname \"${SERVERNAME}.${NETWORK}\";" >> ${SERVERFILE}
+		echo "\tinfo \"`echo ${WORKINGSERVER} | cut -f 6 -d :`\";" >> ${SERVERFILE}
+		echo "\tnumeric `echo ${WORKINGSERVER} | cut -f 7 -d :`" >> ${SERVERFILE}
+		echo "};" >> ${SERVERFILE}
+		echo "drpass {" >> ${SERVERFILE}
+		echo "\trestart \"`echo ${WORKINGSERVER} | cut -f 8 -d :`\" { sha1; };" >> ${SERVERFILE}
+		echo "\tdie \"`echo ${WORKINGSERVER} | cut -f 8 -d :`\" { sha1; };" >> ${SERVERFILE}
+		echo "};" >> ${SERVERFILE}
+	echo "\t- ending ${SERVERFILE}"
+
+done
