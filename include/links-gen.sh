fa4da3c2fec645b5bc959dc3d01bbf6b75a5f058
Dylan Cochran <heliocentric@gmail.com>
2007-03-29 18:50:59 +0000

    links config generation seperated from main.
    yay!
    
    
    git-svn-id: http://gs-config.googlecode.com/svn/trunk@24 b706ddbe-c0eb-11de-acfc-b766374499fb

--- include/links-gen.sh
+++ include/links-gen.sh
@@ -22,4 +22,37 @@
 # HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-# SUCH DAMAGE.
+# SUCH DAMAGE
+links_gen() {
+        for REMOTESERVER in `grep ^S ${STRIPCONF} | grep -v ${SERVERNAME}`
+        do
+		LINKFILE=${CONFPATH}/links.conf
+                LINKNAME=`echo ${REMOTESERVER} | cut -f 2 -d :`
+                LINKIP=`echo ${REMOTESERVER} | cut -f 3 -d : | cut -f 1 -d -`
+                echo "\nlink ${LINKNAME}.${NETWORK} {" >> ${LINKFILE}
+                echo "\tusername *;" >> ${LINKFILE}
+                echo "\thostname ${LINKIP};" >> ${LINKFILE}
+                echo "\tbind-ip ${BINDIP};" >> ${LINKFILE}
+                echo "\tport 4400;" >> ${LINKFILE}
+                echo "\thub *;" >> ${LINKFILE}
+                echo "\tpassword-connect \"wyldryde-l33t-link-password\";" >> ${LINKFILE}
+                echo "\tpassword-recieve \"\$MMeriLxK\$lVYlZHHBGNvcZcZEBw1d/w==\" { md5; };" >> ${LINKFILE}
+                echo "\tclass leaf;" >> ${LINKFILE}
+                echo "\toptions {" >> ${LINKFILE}
+                if [ "${REGION}" != "0" ] ; then
+                        if [ "${REGION}" != "" ] ; then
+                                for HLINES in `grep ^H ${STRIPCONF}`
+                                do
+                                        if [ "`echo ${HLINES} | cut -f 2 -d :`" == ${REGION} ] ; then
+                                                if [ "`echo ${HLINES} | cut -f 3 -d :`" == ${LINKNAME} ] ; then
+                                                        echo "\t\tautoconnect;" >> ${LINKFILE}
+                                                fi
+                                        fi
+                                done
+                        fi
+                fi
+                echo "\t};" >> ${LINKFILE}
+                echo "};\n" >> ${LINKFILE}
+        done  
+
+}
