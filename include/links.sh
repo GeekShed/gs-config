b1b462b6f34fd04c033edde45321fbfc64d784f9
Allan Jude <allan@geekshed.net>
2007-06-09 20:00:15 +0000

    git-svn-id: http://gs-config.googlecode.com/svn/trunk@45 b706ddbe-c0eb-11de-acfc-b766374499fb

--- include/links.sh
+++ include/links.sh
@@ -27,35 +27,61 @@ links_gen ()
 {
 	LINKFILE=${CONFPATH}/links.conf
 	echo "        - starting ${LINKFILE}"
+    MYREGION=`grep ^S ${STRIPCONF} | grep ${SERVERNAME} | cut -d : -f 8`
         for REMOTESERVER in `grep ^S ${STRIPCONF} | grep -v ${SERVERNAME}`
         do
                 LINKNAME=`echo ${REMOTESERVER} | cut -f 2 -d :`
                 LINKIP=`echo ${REMOTESERVER} | cut -f 3 -d : | cut -f 1 -d -`
+                REGION=`echo ${REMOTESERVER} | cut -f 8 -d :`
+                OPTIONS=`echo ${REMOTESERVER} | cut -f 5 -d :`
                 echo "link ${LINKNAME}.${NETWORK} {" >> ${LINKFILE}
                 echo "        username *;" >> ${LINKFILE}
                 echo "        hostname ${LINKIP};" >> ${LINKFILE}
                 echo "        bind-ip ${BINDIP};" >> ${LINKFILE}
                 echo "        port 4400;" >> ${LINKFILE}
-                echo "        hub *;" >> ${LINKFILE}
                 echo "        password-connect \"wyldryde-l33t-link-password\";" >> ${LINKFILE}
                 echo "        password-recieve \"\$MMeriLxK\$lVYlZHHBGNvcZcZEBw1d/w==\" { md5; };" >> ${LINKFILE}
-                echo "        class leaf;" >> ${LINKFILE}
-                echo "        options {" >> ${LINKFILE}
-                if [ "${REGION}" != "0" ] ; then
-                        if [ "${REGION}" != "" ] ; then
-                                for HLINES in `grep ^H ${STRIPCONF}`
-                                do
-                                        if [ "`echo ${HLINES} | cut -f 2 -d :`" == ${REGION} ] ; then
-                                                if [ "`echo ${HLINES} | cut -f 3 -d :`" == ${LINKNAME} ] ; then
-                                                        echo "                autoconnect;" >> ${LINKFILE}
-                                                fi
-                                        fi
-                                done
+                ISAHUB=0
+                ISMYHUB=0
+                for HLINES in `grep ^H ${STRIPCONF}`
+                do
+                    if [ `echo ${HLINES} | cut -d : -f 4` = "${LINKNAME}" ]; then
+                        ISAHUB=1
+                        if [ `echo ${HLINES} | cut -d : -f 2` = "${MYREGION}" ]; then
+                            #this server is the hub for HUBREGION, and MYREGION is HUBREGION so this is my hub
+                            ISMYHUB=1
                         fi
-		else
-			echo "                autoconnect;" >> ${LINKFILE}
+                    fi
+                done
+                if [ "${ISAHUB}" = "1" ]; then
+                    if [ `grep ^H:0 ${STRIPCONF} | cut -d : -f 4` = "${LINKNAME}" ]; then
+                        #this server is the root hub
+                        echo "        class roothub;" >> ${LINKFILE}
+                    else
+                        echo "        class hub;" >> ${LINKFILE}
+                    fi
+                    echo "        hub *;" >> ${LINKFILE}
+                    echo "        options {" >> ${LINKFILE}
+                    if [ "${ISMYHUB}" = "1" ]; then
+                        echo "                autoconnect;" >> ${LINKFILE}
+                    fi
+                    case ${OPTIONS} in *s*) echo "                ssl;" >> ${LINKFILE} ;;	esac
+                    case ${OPTIONS} in *z*) echo "                zip;" >> ${LINKFILE} ;;	esac
+                    echo "        };" >> ${LINKFILE}
+                else
+                    echo "        class leaf;" >> ${LINKFILE}
+                    echo "        hub *;" >> ${LINKFILE}
+                    echo "        options {" >> ${LINKFILE}
+                    if [ `grep ^H:${REGION} ${STRIPCONF} | cut -d : -f 4` = "${SERVERNAME}" ]; then
+                        #i am the hub for this region
+                        #reverse autoconnect
+                        case ${OPTIONS} in *a*) echo "                autoconnect;" >> ${LINKFILE} ;;	esac
+                    fi
+                    case ${OPTIONS} in *s*) echo "                ssl;" >> ${LINKFILE} ;;	esac
+                    case ${OPTIONS} in *z*) echo "                zip;" >> ${LINKFILE} ;;	esac
+                    case ${OPTIONS} in *q*) echo "                quarantine;" >> ${LINKFILE} ;;	esac
+                    echo "        };" >> ${LINKFILE}
                 fi
-                echo "        };" >> ${LINKFILE}
                 echo "};" >> ${LINKFILE}
         done  
 	echo "        - ending ${LINKFILE}"
