9e57d057cf76f95c61d46ce4bd5272a7736fc806
Dylan Cochran <heliocentric@gmail.com>
2007-06-15 06:49:21 +0000

    changed include/conf.sh to support the common config routines.
    added conf_init to conf.sh to set common vars for utilities.
    
    the utilities all run for script in include/* ; do ; . ${script} ; done ; conf_init to get a unified namespace.
    
    touched auto-install and auto-config to reflect this
    
    added username and port to S:Line format and changed example.conf to reflect this
    
    added ossh wrapper to auto-install, ossh SERVERNAME 'command'
    
    added push_server to auto-install, that will tar, send, configure, and make a server
    
    changed usage and case loop for push target
    
    
    
    git-svn-id: http://gs-config.googlecode.com/svn/trunk@54 b706ddbe-c0eb-11de-acfc-b766374499fb

--- include/conf.sh
+++ include/conf.sh
@@ -25,86 +25,20 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
-conf_gen ()
+conf_init ()
 {
-for WORKINGSERVER in `grep ^S ${STRIPCONF}`
-do
-	SERVERNAME=`echo ${WORKINGSERVER} | cut -f 2 -d :`
-	REGION=`echo ${WORKINGSERVER} | cut -f 9 -d :`
-	CONFPATH=${OUTPUTPATH}/conf/${SERVERNAME}
-	PORTSFILE=${CONFPATH}/ports.conf
-	OPERFILE=${CONFPATH}/olines.conf
-	LINKFILE=${CONFPATH}/links.conf
-	SERVERFILE=${CONFPATH}/server.conf
-	echo "Generating config files for ${SERVERNAME}.${NETWORK}"
-	mkdir ${CONFPATH}
-	BINDIP=`echo ${WORKINGSERVER} | cut -f 3 -d : | cut -f 1 -d -`
-	echo "\t- starting ${LINKFILE}"
-	
-	for REMOTESERVER in "`grep ^S ${STRIPCONF} | grep -v ${SERVERNAME}`"
-	do
-		LINKNAME=`echo ${REMOTESERVER} | cut -f 2 -d :`
-		LINKIP=`echo ${REMOTESERVER} | cut -f 3 -d : | cut -f 1 -d -`
-		echo "\nlink ${LINKNAME}.${NETWORK} {" >> ${LINKFILE}
-		echo "\tusername *;" >> ${LINKFILE}
-		echo "\thostname ${LINKIP};" >> ${LINKFILE}
-		echo "\tbind-ip ${BINDIP};" >> ${LINKFILE}
-		echo "\tport 4400;" >> ${LINKFILE}
-		echo "\thub *;" >> ${LINKFILE}
-		echo "\tpassword-connect \"wyldryde-l33t-link-password\";" >> ${LINKFILE}
-		echo "\tpassword-recieve \"\$MMeriLxK\$lVYlZHHBGNvcZcZEBw1d/w==\" { md5; };" >> ${LINKFILE}
-		echo "\tclass leaf;" >> ${LINKFILE}
-		echo "\toptions {" >> ${LINKFILE}
-		if [ "${REGION}" != "0" ] ; then
-			if [ "${REGION}" != "" ] ; then
-				for HLINES in `grep ^H ${STRIPCONF}`
-				do
-					if [ "`echo ${HLINES} | cut -f 2 -d :`" == ${REGION} ] ; then
-						if [ "`echo ${HLINES} | cut -f 3 -d :`" == ${LINKNAME} ] ; then
-							echo "\t\tautoconnect;" >> ${LINKFILE}
-						fi
-					fi 
-				done
-			fi
-		fi
-		echo "\t};" >> ${LINKFILE}
-		echo "};\n" >> ${LINKFILE}
-	done
-	echo "\t- ending ${LINKFILE}"
-	echo "\t- starting ${PORTSFILE}"
-	for LISTENIP in "`echo ${WORKINGSERVER} | cut -f 3 -d : | sed s/-/\ /g | sed s/\;/\:/g`"
-	do
-		for PORTS in `grep ^P ${STRIPCONF}`
-		do
-			PORT=`echo ${PORTS} | cut -f 2 -d :`
-			OPTIONS=`echo ${PORTS} | cut -f 3 -d :`
-			if [ "`echo "${LISTENIP}"| grep -c ^\\\[`" = "0" ] ; then
-				echo "listen [::ffff:${LISTENIP}]:${PORT} {" >> ${PORTSFILE}
-			else
-				echo "listen ${LISTENIP}:${PORT} {" >> ${PORTSFILE}
-			fi
-			if [ "${OPTIONS}" != "" ] ; then
-				echo "\toptions {" >> ${PORTSFILE}
-				case ${OPTIONS} in *c*) echo "\t\tclients-only;" >> ${PORTSFILE} ;;	esac
-				case ${OPTIONS} in *s*) echo "\t\tservers-only;" >> ${PORTSFILE} ;;	esac
-				case ${OPTIONS} in *l*) echo "\t\tssl;" >> ${PORTSFILE} ;;		esac
-				echo "\t};" >> ${PORTSFILE}
-			fi
-			echo "};" >> ${PORTSFILE}
-		done
-	done
-	echo "\t- ending ${PORTSFILE}"
-	echo "\t- starting ${SERVERFILE}"
-		echo "me {" >> ${SERVERFILE}
-		echo "\tname \"${SERVERNAME}.${NETWORK}\";" >> ${SERVERFILE}
-		echo "\tinfo \"`echo ${WORKINGSERVER} | cut -f 6 -d :`\";" >> ${SERVERFILE}
-		echo "\tnumeric `echo ${WORKINGSERVER} | cut -f 7 -d :`" >> ${SERVERFILE}
-		echo "};" >> ${SERVERFILE}
-		echo "drpass {" >> ${SERVERFILE}
-		echo "\trestart \"`echo ${WORKINGSERVER} | cut -f 8 -d :`\" { sha1; };" >> ${SERVERFILE}
-		echo "\tdie \"`echo ${WORKINGSERVER} | cut -f 8 -d :`\" { sha1; };" >> ${SERVERFILE}
-		echo "};" >> ${SERVERFILE}
-	echo "\t- ending ${SERVERFILE}"
-
-done
+        # set var's to use
+        SCRIPTROOT=`pwd`
+        CONFIG=${SCRIPTROOT}/conf/network.conf ; export CONFIG
+        SHELL=/bin/sh ; export SHELL
+        OUTPUTPATH=/tmp/cf-gen ; export OUTPUTPATH
+        rm -rf ${OUTPUTPATH}
+        mkdir -p ${OUTPUTPATH}
+        chmod 750 ${OUTPUTPATH}
+        NAME=${CONFIG} ; export NAME
+        # stripping out comments and whitespace:
+        grep -v ^# ${NAME} | grep -v ^$ | cut -f 1 -d \# > ${OUTPUTPATH}/network.conf
+        STRIPCONF=${OUTPUTPATH}/network.conf ; export STRIPCONF
+        mkdir -p ${OUTPUTPATH}/conf
+        NETWORK=WyldRyde.org ; export NETWORK
 }
