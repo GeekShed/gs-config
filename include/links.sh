80ea2675a68138ca92b224e44f97c1525f95f43a
Dylan Cochran <heliocentric@gmail.com>
2009-11-02 03:44:24 +0000

    * Addd sctp support to the link file generation, now it should work between servers that support it.
    git-svn-id: http://gs-config.googlecode.com/svn/trunk@129 b706ddbe-c0eb-11de-acfc-b766374499fb

--- include/links.sh
+++ include/links.sh
@@ -31,6 +31,8 @@ links_gen ()
 	echo "    - starting ${LINKFILE}"
 	DEFSERVERPORT="$(grep ^N ${STRIPCONF} | cut -d : -f 7)"
 	DEFSSLSERVERPORT="$(grep ^N ${STRIPCONF} | cut -d : -f 8)"
+	DEFSCTPSERVERPORT="$(grep ^N ${STRIPCONF} | cut -d : -f 9)"
+	DEFSCTPSSLSERVERPORT="$(grep ^N ${STRIPCONF} | cut -d : -f 10)"
 	MYREGION=`grep ^S ${STRIPCONF} | grep ${SERVERNAME} | cut -d : -f 8`
 	BINDIP=`grep ^S ${STRIPCONF} | grep ${SERVERNAME} | cut -d : -f 3 | cut -d - -f 1`
 	echo "ulines {" >> ${LINKFILE}
@@ -162,6 +164,148 @@ links_gen ()
 				fi
 				echo "    hub *;" >> ${LINKFILE}
 				echo "    options {" >> ${LINKFILE}
+				echo "        ssl;" >> ${LINKFILE}
+				if [ "${IAMTHEHUB}" = "1" ]; then
+					if [ "${REGION}" = "${MYREGION}" ]; then
+						#i am the hub for this region
+						#reverse autoconnect
+						case ${OPTIONS} in *a*) echo "        autoconnect;" >> ${LINKFILE} ;;	esac
+					fi
+				fi
+				case ${OPTIONS} in *z*) echo "        zip;" >> ${LINKFILE} ;;	esac
+				case ${OPTIONS} in *q*) echo "        quarantine;" >> ${LINKFILE} ;;	esac
+				echo "    };" >> ${LINKFILE}
+			fi
+			echo "};" >> ${LINKFILE}
+		fi
+
+		# For SCTP Servers
+		if [ `echo ${OPTIONS} | grep -c 't'` -eq 1 ]; then
+			echo "link ${LINKNAME}.${DNSSUFFIX} {" >> ${LINKFILE}
+			echo "    username *;" >> ${LINKFILE}
+			echo "    hostname ${LINKIP};" >> ${LINKFILE}
+			echo "    bind-ip ${BINDIP};" >> ${LINKFILE}
+			echo "    port ${DEFSCTPSERVERPORT};" >> ${LINKFILE}
+			echo "    password-connect \"`grep ^X ${STRIPCONF} | cut -d : -f 2`\";" >> ${LINKFILE}
+			echo "    password-receive \"`grep ^X ${STRIPCONF} | cut -d : -f 3`\" { sha1; };" >> ${LINKFILE}
+			ISAHUB=0
+			ISMYHUB=0
+			IAMTHEHUB=0
+			if [ "`grep ^H:${REGION} ${STRIPCONF} | cut -d : -f 4`" = "${SERVERNAME}" ]; then
+				IAMTHEHUB=1
+			fi
+			for HLINES in `grep ^H ${STRIPCONF}`
+			do
+				if [ "`echo ${HLINES} | cut -d : -f 4`" = "${LINKNAME}" ]; then
+					ISAHUB=1
+					if [ "`echo ${HLINES} | cut -d : -f 2`" = "${MYREGION}" ]; then
+						#this server is the hub for HUBREGION, and MYREGION is HUBREGION so this is my hub
+						ISMYHUB=1
+					fi
+				fi
+			done
+			if [ "${ISAHUB}" = "1" ]; then
+				if [ "`grep ^H:0 ${STRIPCONF} | cut -d : -f 4`" = "${LINKNAME}" ]; then
+					#this server is the root hub
+					#echo "    class roothub;" >> ${LINKFILE}
+					#instead, lets treat this as a normal hub for now
+					#so we can use the maxclients=1 to prevent a leaf bridging hubs
+					echo "    class hub;" >> ${LINKFILE}
+				else
+					if [ "${IAMTHEROOTHUB}" = "1" ]; then
+						#i am the root hub, allow multiple hubs to connect to me
+						echo "    class servers;" >> ${LINKFILE}
+					else
+						echo "    class hub;" >> ${LINKFILE}
+					fi
+				fi
+				echo "    hub *;" >> ${LINKFILE}
+				echo "    options {" >> ${LINKFILE}
+				echo "        sctp;" >> ${LINKFILE}
+				if [ "${ISMYHUB}" = "1" ]; then
+					echo "        autoconnect;" >> ${LINKFILE}
+				fi
+				case ${OPTIONS} in *z*) echo "        zip;" >> ${LINKFILE} ;;	esac
+				echo "    };" >> ${LINKFILE}
+			else
+				if [ "`grep ^R: ${STRIPCONF} | cut -d : -f 2`" = "${LINKNAME}" ]; then
+					echo "    class upsendq;" >> ${LINKFILE}
+				else
+					echo "    class leaf;" >> ${LINKFILE}
+				fi
+				echo "    hub *;" >> ${LINKFILE}
+				echo "    options {" >> ${LINKFILE}
+				echo "        sctp;" >> ${LINKFILE}
+				if [ "${IAMTHEHUB}" = "1" ]; then
+					if [ "${REGION}" = "${MYREGION}" ]; then
+						#i am the hub for this region
+						#reverse autoconnect
+						case ${OPTIONS} in *a*) echo "        autoconnect;" >> ${LINKFILE} ;;	esac
+					fi
+				fi
+				case ${OPTIONS} in *z*) echo "        zip;" >> ${LINKFILE} ;;	esac
+				case ${OPTIONS} in *q*) echo "        quarantine;" >> ${LINKFILE} ;;	esac
+				echo "    };" >> ${LINKFILE}
+			fi
+			echo "};" >> ${LINKFILE}
+		fi
+		if [ `echo ${OPTIONS} | grep -c 't'` -eq 1 ]; then
+			echo "link ${LINKNAME}.${DNSSUFFIX} {" >> ${LINKFILE}
+			echo "    username *;" >> ${LINKFILE}
+			echo "    hostname ${LINKIP};" >> ${LINKFILE}
+			echo "    bind-ip ${BINDIP};" >> ${LINKFILE}
+			echo "    port ${DEFSCTPSSLSERVERPORT};" >> ${LINKFILE}
+			echo "    password-connect \"`grep ^X ${STRIPCONF} | cut -d : -f 2`\";" >> ${LINKFILE}
+			echo "    password-receive \"`grep ^X ${STRIPCONF} | cut -d : -f 3`\" { sha1; };" >> ${LINKFILE}
+			ISAHUB=0
+			ISMYHUB=0
+			IAMTHEHUB=0
+			if [ "`grep ^H:${REGION} ${STRIPCONF} | cut -d : -f 4`" = "${SERVERNAME}" ]; then
+				IAMTHEHUB=1
+			fi
+			for HLINES in `grep ^H ${STRIPCONF}`
+			do
+				if [ "`echo ${HLINES} | cut -d : -f 4`" = "${LINKNAME}" ]; then
+					ISAHUB=1
+					if [ "`echo ${HLINES} | cut -d : -f 2`" = "${MYREGION}" ]; then
+						#this server is the hub for HUBREGION, and MYREGION is HUBREGION so this is my hub
+						ISMYHUB=1
+					fi
+				fi
+			done
+			if [ "${ISAHUB}" = "1" ]; then
+				if [ "`grep ^H:0 ${STRIPCONF} | cut -d : -f 4`" = "${LINKNAME}" ]; then
+					#this server is the root hub
+					#echo "    class roothub;" >> ${LINKFILE}
+					#instead, lets treat this as a normal hub for now
+					#so we can use the maxclients=1 to prevent a leaf bridging hubs
+					echo "    class hub;" >> ${LINKFILE}
+				else
+					if [ "${IAMTHEROOTHUB}" = "1" ]; then
+						#i am the root hub, allow multiple hubs to connect to me
+						echo "    class servers;" >> ${LINKFILE}
+					else
+						echo "    class hub;" >> ${LINKFILE}
+					fi
+				fi
+				echo "    hub *;" >> ${LINKFILE}
+				echo "    options {" >> ${LINKFILE}
+				echo "        sctp;" >> ${LINKFILE}
+				echo "        ssl;" >> ${LINKFILE}
+				if [ "${ISMYHUB}" = "1" ]; then
+					echo "        autoconnect;" >> ${LINKFILE}
+				fi
+				case ${OPTIONS} in *z*) echo "        zip;" >> ${LINKFILE} ;;	esac
+				echo "    };" >> ${LINKFILE}
+			else
+				if [ "`grep ^R: ${STRIPCONF} | cut -d : -f 2`" = "${LINKNAME}" ]; then
+					echo "    class upsendq;" >> ${LINKFILE}
+				else
+					echo "    class leaf;" >> ${LINKFILE}
+				fi
+				echo "    hub *;" >> ${LINKFILE}
+				echo "    options {" >> ${LINKFILE}
+				echo "        sctp;" >> ${LINKFILE}
 				echo "        ssl;" >> ${LINKFILE}
 				if [ "${IAMTHEHUB}" = "1" ]; then
 					if [ "${REGION}" = "${MYREGION}" ]; then
